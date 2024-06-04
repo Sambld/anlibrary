cfeb160fdd6a4a3aae91d7d2cd183d3b53ee4da6
Sambld <samibld18@gmail.com>
2024-06-05 00:01:19 +0100

    fix dialog component

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -32,7 +32,7 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, closebutton, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
@@ -42,11 +42,11 @@ const DialogContent = React.forwardRef<
         className
       )}
       {...props}
-      closebutton={closebutton}
+      // closebutton={closebutton}
     >
       {children}
       <DialogPrimitive.Close
-        hidden={!!(closebutton == "hidden")}
+        // hidden={!!(closebutton == "hidden")}
         className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
       >
         <X className="h-4 w-4" />
